cd4d115cd81ed859e750cc357bf96eb96dddd88c
Robin Rai <88548178+Robinrai2612@users.noreply.github.com>
2023-09-17 20:19:30 +0530

    Added solution - LeetHub

---
+++ Kth largest element in BST - GFG/kth-largest-element-in-bst.py
@@ -0,0 +1,104 @@
+class Solution:
+    def kthLargest(self,root, k):
+        def reverse_inorder(node):
+            nonlocal k
+            if node is None or k == 0:
+                return None
+
+            # Traverse the right subtree
+            right_result = reverse_inorder(node.right)
+
+            # If we've already found the Kth largest element in the right subtree,
+            # return it.
+            if right_result is not None:
+                return right_result
+
+            # Decrement K and check if it's now 0. If it is, we've found the Kth
+            # largest element.
+            k -= 1
+            if k == 0:
+                return node.data
+
+            # Otherwise, traverse the left subtree.
+            return reverse_inorder(node.left)
+
+        return reverse_inorder(root)
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+from collections import deque
+# Tree Node
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+
+# Function to Build Tree   
+def buildTree(s):
+    #Corner Case
+    if(len(s)==0 or s[0]=="N"):           
+        return None
+        
+    # Creating list of strings from input 
+    # string after spliting by space
+    ip=list(map(str,s.split()))
+    
+    # Create the root of the tree
+    root=Node(int(ip[0]))                     
+    size=0
+    q=deque()
+    
+    # Push the root to the queue
+    q.append(root)                            
+    size=size+1 
+    
+    # Starting from the second element
+    i=1                                       
+    while(size>0 and i<len(ip)):
+        # Get and remove the front of the queue
+        currNode=q[0]
+        q.popleft()
+        size=size-1
+        
+        # Get the current node's value from the string
+        currVal=ip[i]
+        
+        # If the left child is not null
+        if(currVal!="N"):
+            
+            # Create the left child for the current node
+            currNode.left=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.left)
+            size=size+1
+        # For the right child
+        i=i+1
+        if(i>=len(ip)):
+            break
+        currVal=ip[i]
+        
+        # If the right child is not null
+        if(currVal!="N"):
+            
+            # Create the right child for the current node
+            currNode.right=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.right)
+            size=size+1
+        i=i+1
+    return root
+    
+if __name__=="__main__":
+    t=int(input())
+    for _ in range(0,t):
+        s=input()
+        root=buildTree(s)
+        k = int(input())
+        print (Solution().kthLargest(root, k))
+# } Driver Code Ends
